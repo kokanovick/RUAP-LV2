604cb46e4516db36d37658dc35fad027ede96d3c
Karlo Kokanović <karlo02.12@hotmail.com>
2022-11-13 14:53:21 +0100

    Second task

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        public string[] Get()
+        {
+            return new string[]
+            {
+                "Hello",
+                "World"
+            };
+        }
+    }
+}
